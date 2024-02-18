de1f0ed9d8770d621df98fbf312f590dce757508
Akash Kumar <akashkumar@Akashs-MacBook-Air.local>
2024-02-18 09:02:21 +0530

    cehck warn

--- dist/server.js
+++ dist/server.js
@@ -10,3 +10,6 @@ const server = http_1.default.createServer(app_1.default);
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port} ::[${new Date().toLocaleString()}]`);
 });
+let timer = setInterval(() => {
+    console.log("console for prevewnt auto stop service in server.ts line no.7");
+}, 3000);
