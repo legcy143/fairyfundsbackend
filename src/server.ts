9989538660a5a1fd42b7b2ff029076cac8f1fdfa
Akash Kumar <akashkumar@Akashs-MacBook-Air.local>
2024-02-18 09:01:56 +0530

    check it warn

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import app from "./app";
 const port = process.env.PORT || 5555;
 const server = http.createServer(app)
 
+
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port} ::[${new Date().toLocaleString()}]`)
 }) 
+let timer = setInterval(() => {
+    console.log("console for prevewnt auto stop service in server.ts line no.7")
+}, 3000);
