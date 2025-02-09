3e8aed0f9d53cf8390fd3acecdbac6a192fece57
legcy <legcy143@gmail.com>
2025-02-09 19:50:19 +0000

    secure: admin details

--- src/admin/AdminAuth.ts
+++ src/admin/AdminAuth.ts
@@ -3,7 +3,13 @@ import asyncHandler from "../utils/handler/asyncHandler";
 import { errorResponse, successResponse } from "../utils/response/Response";
 import { GenrateJwtToken } from "../misc/jwt";
 
-let dummy = {_id:566556655665,email :'legcy143@gmail.com', password:"stealit." , otp:121212};
+// let dummy = {_id:566556655665,email :'legcy143@gmail.com', password:"stealit." , otp:121212};
+let dummy = {
+    _id:process.env.ADMIN_ID,
+    email : process.env.ADMIN_EMAIL ,
+    password:process.env.ADMIN_PASSWORD ,
+    otp:process.env.ADMIN_OTP
+}
 
 export const AdminGetOtp = asyncHandler(async(req:Request , res:Response)=>{
     const {email , password } = req.body;
