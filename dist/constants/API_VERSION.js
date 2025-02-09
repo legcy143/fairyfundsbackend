13e43ff2e65ed066392167828d3daf78de987e48
legcy <legcy143@gmail.com>
2025-02-09 20:11:49 +0000

    remove: dist

--- dist/constants/API_VERSION.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_CURRENT_VERSION = void 0;
-exports.API_CURRENT_VERSION = 1;
--- dist/enums/UserRoleEnum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserRoleEnum;
-(function (UserRoleEnum) {
-    UserRoleEnum["Admin"] = "admin";
-    UserRoleEnum["Member"] = "member";
-})(UserRoleEnum || (UserRoleEnum = {}));
-exports.default = UserRoleEnum;
