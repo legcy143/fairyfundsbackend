a329e4394c0148ab6cd497e03124f693ab81a3e2
legcy <legcy143@gmail.com>
2025-02-09 19:41:10 +0000

    fixing: spining time by axios

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import http from "http"
 import app from "./app";
+import axios from "axios";
 const port = process.env.PORT || 5555;
 const server = http.createServer(app)
 
@@ -7,6 +8,15 @@ const server = http.createServer(app)
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port} ::[${new Date().toLocaleString()}]`)
 }) 
-let timer = setInterval(() => {
-    console.log("console for prevewnt auto stop service in server.ts line no.7")
+
+
+
+
+let timer = setInterval(async() => {
+    try {
+        let res = await axios.get("http://localhost:5555"+"/health")
+        console.log("console for prevent auto stop service in server.ts line no.7" , res.data)
+    } catch (error) {
+        console.log("error in server.ts line no.7" , error)        
+    }
 }, 3000);
